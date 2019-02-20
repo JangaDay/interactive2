eb194a3ccb59d1410f8c9468ae55c6f9d0daf7d5
JangaDay <35511337+JangaDay@users.noreply.github.com>
2019-02-19 16:04:21 -0800

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,66 @@
-let letters = document.querySelectorAll('.letter')
+let letters = document.querySelectorAll('.firstpage .letter')
 
-    for(let counter = 0; counter < letters.length; counter ++){
-        letters[counter].addEventListener('mouseenter',function(){
-        	letters[counter].classList.add('beenMoused')
+    
+let doneDidScroll = false
+
+setTimeout(function(){
+	 window.addEventListener('scroll',function(){
+	 	
+	 	if(!doneDidScroll){
+		 	for(let counter = 0; counter < letters.length; counter ++){
+		       
+		    	letters[counter].classList.add('beenMoused')
+		        
+
+		    }
+		}
+
+		doneDidScroll = true
+
+	 })
+	},10)
+
+
+
+
+
+let letters2 = document.querySelectorAll('.centerthings .letter')
+
+    
+let doneDidScroll2 = false
+
+setTimeout(function(){
+	 window.addEventListener('scroll',function(){
+	 	
+	 	if(window.scrollY > window.innerHeight){
+
+
+
+		 	if(!doneDidScroll2){
+			 	for(let counter = 0; counter < letters2.length; counter ++){
+			       
+			    	letters2[counter].classList.add('beenMoused')
+			        
+
+			    }
+			}
+
+			doneDidScroll2 = true
+		}
+	 })
+	},10)
+
+
+
+
+
+
+
+let letters3 = document.querySelectorAll('.lastpage .letter')
+
+    for(let counter = 0; counter < letters3.length; counter ++){
+        letters3[counter].addEventListener('mouseenter',function(){
+        	letters3[counter].classList.add('beenMoused')
         })
 
     }
