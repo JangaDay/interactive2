d382c35116ca730f6bfd6d095637ee0689711876
JangaDay <35511337+JangaDay@users.noreply.github.com>
2019-04-18 16:54:02 -0700

    changes

--- scripts/frykholm.js
+++ scripts/frykholm.js
@@ -10,7 +10,7 @@ let cherry = document.querySelector('.cherry')
 let watermelon = document.querySelector('.watermelon')
 let fruit = document.querySelector('.fruit')
 
-let cherryText = document.querySelector('.cherryText')
+let cherrytext = document.querySelector('.cherrytext')
 
 if(teeth){
 
@@ -53,40 +53,75 @@ window.location.href ="Frykholm%202.html"
 
 } else if(cherry){
 
-	cherry.addEventListener('mouseenter',function(event){
-		watermelon.classList.add('dim')
-		fruit.classList.add('dim')
-		// cherryText.classList.add('show')
-	})
 
-	cherry.addEventListener('mouseleave',function(event){
+
+
+
+	window.addEventListener('mousemove',(e)=>{
+		cherry.classList.remove('hover')
+		fruit.classList.remove('hover')
+		watermelon.classList.remove('hover')
 		watermelon.classList.remove('dim')
 		fruit.classList.remove('dim')
-	})
-
-
-watermelon.addEventListener('mouseenter',function(event){
-		cherry.classList.add('dim')
-		fruit.classList.add('dim')
-		// cherryText.classList.add('show')
-	})
-
-	watermelon.addEventListener('mouseleave',function(event){
 		cherry.classList.remove('dim')
-		fruit.classList.remove('dim')
+		cherrytext.classList.remove('show')
+
+		if(e.clientX < 475){
+			cherry.classList.add('hover')
+			watermelon.classList.add('dim')
+			fruit.classList.add('dim')
+			cherrytext.classList.add('show')
+		} else if(e.clientX < 950){
+			watermelon.classList.add('hover')
+			cherry.classList.add('dim')
+			fruit.classList.add('dim')
+			// watermelontext.classList.add('show')
+		} else {
+			fruit.classList.add('hover')
+			cherry.classList.add('dim')
+			watermelon.classList.add('dim')
+		}
 	})
 
 
-	fruit.addEventListener('mouseenter',function(event){
-		cherry.classList.add('dim')
-		watermelon.classList.add('dim')
-		// cherryText.classList.add('show')
-	})
 
-	fruit.addEventListener('mouseleave',function(event){
-		cherry.classList.remove('dim')
-		watermelon.classList.remove('dim')
-	})
+
+
+
+// 	cherry.addEventListener('mouseenter',function(event){
+// 		watermelon.classList.add('dim')
+// 		fruit.classList.add('dim')
+// 		cherrytext.classList.add('show')
+// 	})
+
+// 	cherry.addEventListener('mouseleave',function(event){
+// 		watermelon.classList.remove('dim')
+// 		fruit.classList.remove('dim')
+// 	})
+
+
+// watermelon.addEventListener('mouseenter',function(event){
+// 		cherry.classList.add('dim')
+// 		fruit.classList.add('dim')
+// 		// cherryText.classList.add('show')
+// 	})
+
+// 	watermelon.addEventListener('mouseleave',function(event){
+// 		cherry.classList.remove('dim')
+// 		fruit.classList.remove('dim')
+// 	})
+
+
+// 	fruit.addEventListener('mouseenter',function(event){
+// 		cherry.classList.add('dim')
+// 		watermelon.classList.add('dim')
+// 		// cherryText.classList.add('show')
+// 	})
+
+// 	fruit.addEventListener('mouseleave',function(event){
+// 		cherry.classList.remove('dim')
+// 		watermelon.classList.remove('dim')
+// 	})
 
 }
 
